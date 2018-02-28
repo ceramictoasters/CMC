b7e90f8008d84c2a6a472bc0762c6af25340c230
RMORRIS001 <rmorris001@vplincsci051.ad.csbsju.edu>
2018-02-28 15:38:52 -0600

    AdminFC bug fixes

--- src/AdminFunctionalityController.java
+++ src/AdminFunctionalityController.java
@@ -2,85 +2,89 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 /**
- * AdminFunctionalityController 
+ * AdminFunctionalityController
+ * 
  * @Version February 27, 2018
  */
-public class AdminFunctionalityController
-{
-	
-/**
- * @return schools
- */
+public class AdminFunctionalityController {
+
+	/**
+	 * @return schools
+	 */
 	DBController dBController = new DBController();
 	UserFunctionalityController accountController = new UserFunctionalityController(null);
-  public ArrayList<School> viewSchools()
-  {
-    return dBController.getAllSchool();
-  }
-  /**
-   * returns a school 
-   * @param name: String
-   * @return school
-   */
-  public School viewSchool(String name)
-  {
-   
-    return dBController.getSchool(name);
-  }
-  /**
-   * Adds a new school 
-   * @param school
-   * @return newSchool
-   */
-  public boolean addNewSchool(String schoolName, String state,String location, String control, 
-		  						int numStudents, double percentFemale, int verbalSAT, int mathSAT, double expense, 
-		  						double percentFinancialAid, int numApplicants, double percentAdmit, double percentEnroll, 
-		  						int academicScale, int socialScale, int qualityOfLifeScale, Collection<String> areaOfStudy, 
-		  						String emphasis)
-  {
-     boolean newSchool = dBController.addNewSchool(schoolName,state,location, control, 
-				numStudents, percentFemale, verbalSAT, mathSAT, expense, 
-				percentFinancialAid, numApplicants, percentAdmit, percentEnroll, 
-				academicScale,socialScale, qualityOfLifeScale, areaOfStudy, 
-				emphasis);
-     return newSchool;
-  }
-  /**
-   * Returns an array of accounts
-   * @return user
-   */
-  public ArrayList<User> viewAccounts()
-  {
-	 return dBController.getUsers();
-  }
-  /**
-   * Creates a new user 
-   * @param firstName
-   * @param lastName
-   * @param username
-   * @param password
-   * @param type
-   * 
-   */
-  public void addNewUser(String firstName, String lastName, String username, String password, char type)
-  {
-    //accountController.createUser(firstName,lastName,username, password,type);
-  }
-  /**
-   * Deactivates a user with a given user
-   * @param user
-   * 
-   */
-  public void deactivateUser(Account user)
-  {
-    //user.accountController.deactivate();
-  }
-  /**
-   * Views an account
-   * @param user
-   */
-  public User viewAccount(String name )
-  {
-    return dBController.getAccount(name);
-  }
+
+	public ArrayList<School> viewSchools() {
+		return dBController.getAllSchool();
+	}
+
+	/**
+	 * returns a school
+	 * 
+	 * @param name:
+	 *            String
+	 * @return school
+	 */
+	public School viewSchool(String name) {
+
+		return dBController.getSchool(name);
+	}
+
+	/**
+	 * Adds a new school
+	 * 
+	 * @param school
+	 * @return newSchool
+	 */
+	public boolean addNewSchool(String schoolName, String state, String location, String control, int numStudents,
+			double percentFemale, int verbalSAT, int mathSAT, double expense, double percentFinancialAid,
+			int numApplicants, double percentAdmit, double percentEnroll, int academicScale, int socialScale,
+			int qualityOfLifeScale, Collection<String> areaOfStudy, String emphasis) {
+		boolean newSchool = dBController.addNewSchool(schoolName, state, location, control, numStudents, percentFemale,
+				verbalSAT, mathSAT, expense, percentFinancialAid, numApplicants, percentAdmit, percentEnroll,
+				academicScale, socialScale, qualityOfLifeScale, areaOfStudy, emphasis);
+		return newSchool;
+	}
+
+	/**
+	 * Returns an array of accounts
+	 * 
+	 * @return user
+	 */
+	public ArrayList<User> viewAccounts() {
+		return dBController.getUsers();
+	}
+
+	/**
+	 * Creates a new user
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param username
+	 * @param password
+	 * @param type
+	 * 
+	 */
+	public void addNewUser(String firstName, String lastName, String username, String password, char type) {
+		// accountController.createUser(firstName,lastName,username, password,type);
+	}
+
+	/**
+	 * Deactivates a user with a given user
+	 * 
+	 * @param user
+	 * 
+	 */
+	public void deactivateUser(Account user) {
+		// user.accountController.deactivate();
+	}
+
+	/**
+	 * Views an account
+	 * 
+	 * @param user
+	 */
+	public User viewAccount(String name) {
+		// return dBController.getAccount(name);
+	}
 }
