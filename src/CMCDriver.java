310a0236519239829e35c5945293a0cf5c7a692a
TSWILSON <tswilson@vplinmath09.ad.csbsju.edu>
2018-03-20 21:20:06 -0500

    Added more layout to the driver

--- src/CMCDriver.java
+++ src/CMCDriver.java
@@ -66,11 +66,11 @@ public class CMCDriver {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Enter Username:");
-		String username = scan.next();
-		System.out.println("\nEnter Password: ");
-		String password = scan.next();
-		LogOn.run(username, password);
+//		System.out.println("Enter Username:");
+//		String username = scan.next();
+//		System.out.println("\nEnter Password: ");
+//		String password = scan.next();
+//		LogOn.run(username, password);
 		
 		AdminFunctionalityController adminFC = new AdminFunctionalityController();
 		DBController dbController = new DBController();
@@ -85,33 +85,48 @@ public class CMCDriver {
 		}
 
 		/**
-		 * Tests
+		 * Add a school
 		 */
-				School newSchool = new School("verbum", "null1", "null2", "null3", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, null);
-				Boolean addSchool = adminFC.addNewSchool(newSchool);
-				System.out.println(addSchool);
+//		School newSchool = new School("verbum", "null1", "null2", "null3", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, null);
+//		Boolean addSchool = adminFC.addNewSchool(newSchool);
+//		System.out.println(addSchool);
+//		System.out.println("----------------------------");
+		/**
+		 * Removes a school
+		 */
+//		School verbum = dbController.getSchool("verbum");
+//		Boolean removedSchool = adminFC.removeSchool(verbum);
+//		System.out.println(removedSchool);
+//		System.out.println("----------------------------");
+		/**
+		 * Views all the schools
+		 */
+		System.out.println("------------View all the schools----------------");
+		schools = adminFC.viewSchools();
+		System.out.println(schools);
+		/**
+		 * Views a specific school
+		 */
+		System.out.println("--------View the school 'YALE'----------");
+		School school = adminFC.viewSchool("YALE");
+		System.out.println(school);
 
-				School verbum = dbController.getSchool("verbum");
-				Boolean removedSchool = adminFC.removeSchool(verbum);
-				System.out.println(removedSchool);
+		/**
+		 * Views all the accounts		
+		 */
+		System.out.println("-----View all the accounts----------");
+		accounts = adminFC.viewAccounts();
+		System.out.println(accounts);
 
-				schools = adminFC.viewSchools();
-				System.out.println(schools);
-				
-				School school = adminFC.viewSchool("verbum");
-				System.out.println(school);
-				
-				accounts = adminFC.viewAccounts();
-				System.out.println(accounts);
-				
-				Account account = adminFC.viewAccount("John");
-				System.out.println(account.getUsername());
+		/**
+		 * Views a specific account		
+		 */
+		System.out.println("------View John account-------");
+		Account account = adminFC.viewAccount("John");
+		System.out.println(account.getUsername());
 	}
 
 	public void userLogin() {
-		
-		
-		
-		
+	
 	}
 }
