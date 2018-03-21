0354bcaa0f37a75d23209e7196560a587da45c2e
TSWILSON <tswilson@vplinmath09.ad.csbsju.edu>
2018-03-20 21:51:18 -0500

    Update the driver

--- src/CMCDriver.java
+++ src/CMCDriver.java
@@ -74,60 +74,7 @@ public class CMCDriver {
 //		System.out.println("\nEnter Password: ");
 //		String password = scan.next();
 //		LogOn.run(username, password);
-		
-//		AdminFunctionalityController adminFC = new AdminFunctionalityController();
-//		DBController dbController = new DBController();
-//		ArrayList<School> schools = new ArrayList<School>();
-//		ArrayList<Account> accounts = new ArrayList<Account>();
-//		ArrayList<School> allSchools = dbController.getAllSchools();
-//
-//
-//
-//		for (School mySchool : allSchools){
-//			System.out.println(mySchool.getName());
-//		}
-//
-//		/**
-//		 * Add a school
-//		 */
-////		School newSchool = new School("verbum", "null1", "null2", "null3", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, null);
-////		Boolean addSchool = adminFC.addNewSchool(newSchool);
-////		System.out.println(addSchool);
-////		System.out.println("----------------------------");
-//		/**
-//		 * Removes a school
-//		 */
-////		School verbum = dbController.getSchool("verbum");
-////		Boolean removedSchool = adminFC.removeSchool(verbum);
-////		System.out.println(removedSchool);
-////		System.out.println("----------------------------");
-//		/**
-//		 * Views all the schools
-//		 */
-//		System.out.println("------------View all the schools----------------");
-//		schools = adminFC.viewSchools();
-//		System.out.println(schools);
-//		/**
-//		 * Views a specific school
-//		 */
-//		System.out.println("--------View the school 'YALE'----------");
-//		School school = adminFC.viewSchool("YALE");
-//		System.out.println(school);
-//
-//		/**
-//		 * Views all the accounts		
-//		 */
-//		System.out.println("-----View all the accounts----------");
-//		accounts = adminFC.viewAccounts();
-//		System.out.println(accounts);
-//
-//		/**
-//		 * Views a specific account		
-//		 */
-//		System.out.println("------View John account-------");
-//		Account account = adminFC.viewAccount("John");
-//		System.out.println(account.getUsername());
-		
+	
 		//Add Test User
 		testDBController.addAccount(testUser);
 		//Add Test Account
@@ -144,7 +91,58 @@ public class CMCDriver {
 	}
 	
 	public static void adminDriver(){
-		
+		AdminFunctionalityController adminFC = new AdminFunctionalityController();
+		DBController dbController = new DBController();
+		ArrayList<School> schools = new ArrayList<School>();
+		ArrayList<Account> accounts = new ArrayList<Account>();
+		ArrayList<School> allSchools = dbController.getAllSchools();
+
+
+
+		for (School mySchool : allSchools){
+			System.out.println(mySchool.getName());
+		}
+
+		/**
+		 * Add a school
+		 */
+//		School newSchool = new School("verbum", "null1", "null2", "null3", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, null);
+//		Boolean addSchool = adminFC.addNewSchool(newSchool);
+//		System.out.println(addSchool);
+//		System.out.println("----------------------------");
+		/**
+		 * Removes a school
+		 */
+//		School verbum = dbController.getSchool("verbum");
+//		Boolean removedSchool = adminFC.removeSchool(verbum);
+//		System.out.println(removedSchool);
+//		System.out.println("----------------------------");
+		/**
+		 * Views all the schools
+		 */
+		System.out.println("------------View all the schools----------------");
+		schools = adminFC.viewSchools();
+		System.out.println(schools);
+		/**
+		 * Views a specific school
+		 */
+		System.out.println("--------View the school 'YALE'----------");
+		School school = adminFC.viewSchool("YALE");
+		System.out.println(school);
+
+		/**
+		 * Views all the accounts		
+		 */
+		System.out.println("-----View all the accounts----------");
+		accounts = adminFC.viewAccounts();
+		System.out.println(accounts);
+
+		/**
+		 * Views a specific account		
+		 */
+		System.out.println("------View John account-------");
+		Account account = adminFC.viewAccount("John");
+		System.out.println(account.getUsername());
 	}
 
 }
