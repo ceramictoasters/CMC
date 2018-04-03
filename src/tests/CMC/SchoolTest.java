d90034b747badf5e45a57af6f4438f955f856edc
TSWILSON <tswilson@vplincsci032.ad.csbsju.edu>
2018-04-02 19:32:27 -0500

    Add SchoolTest and SchoolControllerTest to github

---
+++ src/tests/CMC/SchoolTest.java
@@ -0,0 +1,19 @@
+package tests.CMC;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SchoolTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
